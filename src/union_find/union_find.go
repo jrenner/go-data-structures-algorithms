9637419a2699324f09fb526560576806905efca5
Jon Renner <rennerjc@gmail.com>
2013-05-30 13:39:21 +0800

    more UF work

--- src/union_find/union_find.go
+++ src/union_find/union_find.go
@@ -2,10 +2,23 @@ package union_find
 
 import (
 	"fmt"
-	"os"
 )
 
-const UF_SIZE = 100
+// three implementations of union find
+// quick find, quick union, and weighted quick union
+
+const UF_SIZE = 10
+
+var dataSet = []int{ // 2 component groups
+	4, 3,
+	3, 8,
+	6, 5,
+	9, 4,
+	2, 1,
+	5, 0,
+	7, 2,
+	6, 1,
+}
 
 type UF struct {
 	id []int // access to component id
@@ -22,24 +35,33 @@ func createUF() *UF{
 	return uf
 }
 
-func (uf *UF) find(p int) int {
-	return uf.find(p)
+
+// quick find implementation ---------------------
+func quickFind(uf *UF) {
+	for i := 0; i < UF_SIZE; i = i + 2 {
+		p := dataSet[i]
+		q := dataSet[i+1]
+		if uf.QF_connected(p, q) {
+			continue
+		}
+		uf.QF_union(p, q)
+		fmt.Println("union:", p, q)
+	}
+	fmt.Println(uf.count, "components in data set")
 }
 
-func (uf *UF) areConnected(p, q int) bool {
-	return uf.find(p) == uf.find(q)
+func (uf *UF) QF_find(p int) int {
+	return uf.id[p]
 }
 
-func (uf *UF) union(p, q int) {
+func (uf *UF) QF_union(p, q int) {
 	// put p and q together
-	pID := uf.find(p)
-	qID := uf.find(q)
-
+	pID := uf.QF_find(p)
+	qID := uf.QF_find(q)
 	// If they are together, do nothing
 	if pID == qID {
 		return
 	}
-
 	// Otherwise make p link to q
 	for i := 0; i < len(uf.id); i++ {
 		if uf.id[i] == pID {
@@ -49,13 +71,13 @@ func (uf *UF) union(p, q int) {
 	uf.count--
 }
 
-func getStdinInt() int, err {
-
+func (uf *UF) QF_connected(p, q int) bool {
+	return uf.QF_find(p) == uf.QF_find(q)
 }
+// end quick find ---------------------------
 
 func Run() {
 	uf := createUF()
 	fmt.Printf("created Union Find data set of size: %v\n", uf.count)
-
-	n, err := getStdinInt()
+	quickFind(uf)
 }
